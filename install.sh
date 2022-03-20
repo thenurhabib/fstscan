d7b11e2469b6e64307cd4afe7b5d04ddc9a28ac8
Md. Nur habib <68025241+thenurhabib@users.noreply.github.com>
2022-03-20 18:48:10 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo apt-get update -y
-sudo apt-get install sqlmap -y
+sudo apt-get update -y # update system
+sudo apt-get install sqlmap -y # crawl and find swli
 sudo apt install nikto -y # scan server
 sudo apt-get install nmap -y # Network Scan.
-sudo apt install subfinder -y # SUb find
-sudo apt install skipfish -y # skipfish -o fstscan google.com
+sudo apt install subfinder -y # Sub find
+sudo apt install skipfish -y # vulnerability scanning 
